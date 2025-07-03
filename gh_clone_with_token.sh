78200f8a1c02704fcfd839906d80743f532f7297
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-07-03 15:45:12 +0200

    Delete gh_clone_with_token.sh

--- gh_clone_with_token.sh
+++
@@ -1,9 +0,0 @@
-gh_clone_with_token() {
-    url=$1
-    # Extract the organization and repository name from the URL
-    org_repo=$(echo $url | sed -E 's/.*github.com\/([^\/]+\/[^\/]+).git/\1/')
-    # Authenticate with your second GitHub account
-    gh auth login --with-token < ~/.ghtoken2
-    # Clone the repository using the extracted organization and repository name
-    gh repo clone $org_repo
-}
